87acfc62321663339beae9aeea1e82b36530c902
William Chong <me@williamchong.cloud>
2025-02-09 19:31:14 +0800

    🔧 Add i18n config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US' },
+      { code: 'zh', language: 'zh-TW' }
+    ],
+    defaultLocale: 'en',
+  }
 })
