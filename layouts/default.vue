24b3a952db313bf5c86846b5edbf1f0ee71515ef
William Chong <me@williamchong.cloud>
2025-06-22 05:41:28 +0800

    🏷️ Fix type

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@ useHead({
   script: [
     {
       type: 'application/ld+json',
-      children: JSON.stringify({
+      innerHTML: JSON.stringify({
         '@context': 'https://schema.org',
         '@type': 'WebApplication',
         name: t('title'),
