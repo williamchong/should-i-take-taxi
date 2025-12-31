2e09b7ce74527ef6c8470679383cb3e8021b3bb3
William Chong <me@williamchong.cloud>
2026-01-01 03:33:15 +0800

    💄 Change bg color in dark mode

--- app.vue
+++ app.vue
@@ -5,7 +5,26 @@
 </template>
 
 <script setup lang="ts">
-const { initializeTheme } = useDarkMode()
+const { initializeTheme, isDark } = useDarkMode()
+
+useHead({
+  htmlAttrs: {
+    style: computed(() => isDark.value ? 'background-color: #111827; color-scheme: dark;' : 'background-color: #ffffff; color-scheme: light;')
+  },
+  bodyAttrs: {
+    style: computed(() => isDark.value ? 'background-color: #111827;' : 'background-color: #ffffff;')
+  },
+  meta: [
+    {
+      name: 'color-scheme',
+      content: 'light dark'
+    },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#111827' : '#ffffff')
+    }
+  ]
+})
 
 onMounted(() => {
   initializeTheme()
